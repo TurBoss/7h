185de31ab917506888eb207ba559de5e5b318bef
unab0mb <dave.clark.w@outlook.com>
2019-11-15 19:03:56 -0700

    Don't save dupe entries in Extra Folders to prevent performance degradation, fix dupe direct/music folders on 1st run, ensure direct/music always in configuration per EQ2Alyza's guide.

--- 7thHeaven.Code/Sys.cs
+++ 7thHeaven.Code/Sys.cs
@@ -138,7 +138,8 @@ namespace Iros._7th.Workshop {
                 Settings.Options = GeneralOptions.AutoImportMods;
             }
 
-            if (Settings.ExtraFolders == null || Settings.ExtraFolders.Count == 0) Settings.ExtraFolders = new List<string>() { "direct", "music" };
+            //Fixes dupe direct/music folders on 1st run.
+            //if (Settings.ExtraFolders == null || Settings.ExtraFolders.Count == 0) Settings.ExtraFolders = new List<string>() { "direct", "music" };
 
             string lfile = System.IO.Path.Combine(SysFolder, "library.xml");
             if (System.IO.File.Exists(lfile)) {
