234059ec515460232cea16626f7b869a7b0fc179
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2019-11-29 11:49:42 +0100

    Allow everybody to build it locally again
    Star is not a valid versioning scheme for Visual Studio

--- TurBoLog/AssemblyInfo.cs
+++ TurBoLog/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("--TurBoLoG--")]
 [assembly: AssemblyDescription("FF7 Debug View")]
-[assembly: AssemblyVersion("*")]
-[assembly: AssemblyFileVersion("*")]
-[assembly: AssemblyInformationalVersion("*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
