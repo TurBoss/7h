33d44c928518205cfa0d1a62ffe14665f202cfa0
Martin Barker <martin-barker@hotmail.co.uk>
2021-06-26 16:33:40 +0100

    Added support for reading support resolutions from the WIM system (#18)
    Co-authored-by: Martin Barker <martin.barker@thisisbeacon.com>

---
+++ SeventhHeavenUI/Classes/SupportedResolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeventhHeaven.Classes
+{
+    public class SupportedResolution
+    {
+        public UInt32 H { get; private set; }
+        public UInt32 V { get; private set; }
+
+        private SupportedResolution(UInt32 h, UInt32 v)
+        {
+            H = h;
+            V = v;
+        }
+
+        private static Dictionary<string, SupportedResolution> cache = new Dictionary<string, SupportedResolution>();
+
+        public static SupportedResolution CreateIfNotExist(UInt32 h, UInt32 v)
+        {
+            string key = $"{h}x{v}";
+            if (!cache.ContainsKey(key))
+            {
+                cache[key] = new SupportedResolution(h, v);
+            }
+            return cache[key];
+        }
+
+        public static List<SupportedResolution> GetAllCachedResolutions()
+        {
+            return cache.Values.ToList();
+        }
+    }
+}
