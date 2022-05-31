966bf8fc97c80a579987704cc61413b7bfeae9b2
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2022-05-31 18:41:48 +0200

    PrimaryScreen: Get the resolution of the selected primary monitor on Windows Settings
    This will be the same one the game will use as well

--- SeventhHeavenUI/Classes/PrimaryScreen.cs
+++ SeventhHeavenUI/Classes/PrimaryScreen.cs
@@ -1,32 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace SeventhHeaven.Classes
 {
     public class PrimaryScreen
     {
+        // See https://stackoverflow.com/a/744609
+        [DllImport("user32.dll")]
+        public static extern bool EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEVMODE
+        {
+            private const int CCHDEVICENAME = 0x20;
+            private const int CCHFORMNAME = 0x20;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public ScreenOrientation dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+
+        }
 
         public static List<SupportedResolution> GetSupportedResolutions()
         {
-            var scope = new System.Management.ManagementScope();
-            var q = new System.Management.ObjectQuery("SELECT * FROM CIM_VideoControllerResolution");
-
-            UInt32 maxHResolution;
-            UInt32 maxVResolution;
-
-            using (var searcher = new System.Management.ManagementObjectSearcher(scope, q))
+            DEVMODE vDevMode = new DEVMODE();
+            int i = 0;
+            while (EnumDisplaySettings(null, i, ref vDevMode))
             {
-                var results = searcher.Get();
-
-                foreach (var item in results)
-                {
-                    maxHResolution = (UInt32)item["HorizontalResolution"];
-                    maxVResolution = (UInt32)item["VerticalResolution"];
-                    SupportedResolution.CreateIfNotExist(maxHResolution, maxVResolution);
-                }
+                SupportedResolution.CreateIfNotExist((uint)vDevMode.dmPelsWidth, (uint)vDevMode.dmPelsHeight);
+                i++;
             }
 
             return SupportedResolution.GetAllCachedResolutions();
